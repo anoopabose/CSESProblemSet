83321406460aa4549a8a15573f2438ea82b7e06b
anoopabose <20487395+anoopabose@users.noreply.github.com>
2023-10-06 20:29:50 +0100

    Update Repetitions.cs

--- CSESProblemSet/Introductory.Problems/Repetitions.cs
+++ CSESProblemSet/Introductory.Problems/Repetitions.cs
@@ -4,9 +4,23 @@ public class Repetitions
 {
 
 
-    // You are given a DNA sequence: a string consisting of characters A, C, G, and T. 
-    //Your task is to find the longest repetition in the sequence. 
-    // This is a maximum-length substring containing only one type of character.
+   /*
+You are given a DNA sequence: a string consisting of characters A, C, G, and T. 
+Your task is to find the longest repetition in the sequence. 
+This is a maximum-length substring containing only one type of character.
+Input
+The only input line contains a string of n characters.
+Output
+Print one integer: the length of the longest repetition.
+Constraints
+•	1≤n≤1061≤n≤106
+Example
+Input:
+ATTCGGGA
+Output:
+3
+
+   */
     public static int SolveLongestRepition(string s)
     {
         int max = 0;
