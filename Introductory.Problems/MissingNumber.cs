861d5454c15f5af69bef66e2c965098f901d9401
Bose, Anoop Aravinda <anoop_aravinda_bose@dell.com>
2023-10-06 18:56:21 +0100

    documentain updated

--- Introductory.Problems/MissingNumber.cs
+++ Introductory.Problems/MissingNumber.cs
@@ -1,3 +1,20 @@
+
+/*
+You are given all numbers between 1,2,…,n except one. Your task is to find the missing number.
+Input
+The first input line contains an integer n.
+The second line contains n−1 numbers. Each number is distinct and between 1 and n (inclusive).
+Output
+Print the missing number.
+Constraints
+•	2≤n≤2⋅10^5
+Example
+Input: 
+5
+2 3 1 5
+Output: 4
+
+*/
 public class MissingNumber
 {
     public static int Solve(int n, int[] input)
@@ -5,7 +22,6 @@ public class MissingNumber
         Console.WriteLine("Input : " + Array.ConvertAll(input, x => x.ToString()).Aggregate((x, y) => x + " " + y));
         int sum = input.Aggregate((x, y) => x + y);
         return (n * (n + 1) / 2) - sum;
-
     }
 
 }
